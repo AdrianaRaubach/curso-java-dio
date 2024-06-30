81219e76edccffcdddeb2140bdf8b082dd2e2c67
AdrianaRaubach <raubach.adrianadw@gmail.com>
2024-06-30 14:20:05 -0300

    laços de repetição for

---
+++ controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int contagem = 1; contagem <=20; contagem++) {
+            System.out.println("Contagem: " + contagem);
+        }
+    }
+}
