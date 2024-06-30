6d423dbbefea5244c9087beec655c433bf7a48f8
AdrianaRaubach <raubach.adrianadw@gmail.com>
2024-06-30 12:35:40 -0300

    condição ternária

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,15 +1,10 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 8;
 
-        if(nota>=7)
-        System.out.println("Aprovado");
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
 
-        else if(nota >= 5 && nota < 7)
-        System.out.println("Recuperação");
-
-        else
-        System.out.println("Reprovado");
+        System.out.println(resultado);
     }
     
 }
